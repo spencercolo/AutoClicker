287286fe076d6fc40037f585a64b762ac6320fdb
spencercolo <spencercologna32@gmail.com>
2025-07-03 00:19:01 -0600

    Added a new hook for monitoring the keyboard inputs so we can broadcast a message when a key is pressed

--- Clicker/main.cpp
+++ Clicker/main.cpp
@@ -6,7 +6,7 @@ typedef void (*HookFunc)();
 HMODULE g_hDll = NULL;
 
 int main() {
-	
+
 
 	g_hDll = LoadLibrary(L"x64\\Debug\\WindowsHook.dll");
 	if (!g_hDll) {
@@ -15,26 +15,42 @@ int main() {
 	}
 
 
-	HookFunc SetHook = (HookFunc)GetProcAddress(g_hDll, "SetHook");
-	HookFunc RemoveHook = (HookFunc)GetProcAddress(g_hDll, "RemoveHook");
+	HookFunc SetMouseHook = (HookFunc)GetProcAddress(g_hDll, "SetMouseHook");
+	HookFunc RemoveMouseHook = (HookFunc)GetProcAddress(g_hDll, "RemoveMouseHook");
 
-	if (SetHook && RemoveHook) {
+	HookFunc SetKeyboardHook = (HookFunc)GetProcAddress(g_hDll, "SetKeyboardHook");
+	HookFunc RemoveKeyboardHook = (HookFunc)GetProcAddress(g_hDll, "RemoveKeyboardHook");
 
-		SetHook();
-		std::cout << "Hook Set" << std::endl;
+
+	if (SetMouseHook && RemoveMouseHook && SetKeyboardHook && RemoveKeyboardHook) {
+
+		SetMouseHook();
+		std::cout << "Mouse Hook Set" << std::endl;
+
+		SetKeyboardHook();
+		std::cout << "Keyboard Hook Set" << std::endl;
 
 		MSG msg;
 		while (GetMessage(&msg, NULL, 0, 0)) {
+			std::cout << "message received" << std::endl;
 			if (GetAsyncKeyState(VK_ESCAPE))
 				break;
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 
-		RemoveHook();
-		std::cout << "Hook Removed" << std::endl;
+		RemoveMouseHook();
+		std::cout << "Mouse Hook Removed" << std::endl;
+
+		RemoveKeyboardHook();
+		std::cout << "Keyboard Hook Removed" << std::endl;
+
 		FreeLibrary(g_hDll);
 		std::cout << "Library Freed" << std::endl;
+	}
+
+	else {
+		std::cout << "failed to set hooks" << std::endl;
 	}
 
 	return 0;
--- WindowsHook/MouseHook.cpp
+++ WindowsHook/MouseHook.cpp
@@ -1,9 +1,14 @@
 #include "pch.h"
 #include <windows.h>
+#include <iostream>
 
 
 HHOOK g_hHook = NULL;
 HINSTANCE g_hInstance = NULL;
+
+HHOOK k_hHook = NULL;
+HINSTANCE k_hInstance = NULL;
+
 
 
 //Hook Procedure
@@ -11,26 +16,55 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	//Only process if the wParam and lParam parameters contain information about a mouse message
 	if (nCode == HC_ACTION) {
 		MSLLHOOKSTRUCT* pMouse = (MSLLHOOKSTRUCT*)lParam;
-
 		//Only display if the right mouse button is clicked
 		if (wParam == WM_RBUTTONDOWN) {
-			MessageBox(NULL, L"Right Mouse CLick", L"Mouse Hook", MB_OK);
+			//MessageBox(NULL, L"Right Mouse CLick", L"Mouse Hook", MB_OK);
+			std::cout << "Right Click" << std::endl;
+			//PostMessage(HWND_BROADCAST, nCode, wParam, lParam);
 		}
-	} 
+
+		else if (wParam == WM_KEYDOWN) {
+			std::cout << "Key Down" << std::endl;
+			PostMessage(HWND_BROADCAST, WM_KEYDOWN, wParam, lParam);
+		}
+	}
 	//passes hook information to the next hook procedure
 	return CallNextHookEx(g_hHook, nCode, wParam, lParam);
 }
 
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode == HC_ACTION) {
+		std::cout << "Keyboard Click" << std::endl;
+		std::cout << wParam << std::endl;
+		if (wParam == 'j')
+			std::cout << "Button F Clicked" << std::endl;
+	}
 
-//Hook Installation Function
-extern "C" __declspec(dllexport) void SetHook() {
+	return CallNextHookEx(k_hHook, nCode, wParam, lParam);
+}
+
+extern "C" __declspec(dllexport) void SetKeyboardHook() {
+	if (!k_hHook)
+		k_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, k_hInstance, 0);
+}
+
+extern "C" __declspec(dllexport) void RemoveKeyboardHook() {
+	if (k_hHook) {
+		UnhookWindowsHookEx(k_hHook);
+		k_hHook = NULL;
+	}
+}
+
+
+//Mouse Hook Installation Function
+extern "C" __declspec(dllexport) void SetMouseHook() {
 	//check if null
-	if(!g_hHook)
+	if (!g_hHook)
 		g_hHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, g_hInstance, 0);
 }
 
-//Hook Removal Function
-extern "C" __declspec(dllexport) void RemoveHook() {
+//Mouse Hook Removal Function
+extern "C" __declspec(dllexport) void RemoveMouseHook() {
 	//check if not null
 	if (g_hHook) {
 		UnhookWindowsHookEx(g_hHook);
@@ -40,8 +74,10 @@ extern "C" __declspec(dllexport) void RemoveHook() {
 
 //DLL Entry Point
 BOOL APIENTRY DLLMAIN(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		g_hInstance = hModule;
+		k_hInstance = hModule;
+	}
 
 	return TRUE;
 }
